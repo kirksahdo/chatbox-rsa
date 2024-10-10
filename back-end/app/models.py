ee2f10b067dbba306431326c9ff2814081cd6d69
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-10 18:20:29 -0400

    feat: add models

---
+++ back-end/app/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    public_key = Column(String) 
+    encrypted_private_key = Column(String)
+    
+    
+    sent_messages = relationship("Message", foreign_keys="[Message.sender_id]", back_populates="sender")
+    received_messages = relationship("Message", foreign_keys="[Message.recipient_id]", back_populates="recipient")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    recipient_id = Column(Integer, ForeignKey("users.id"))
+    encrypted_message = Column(String) 
+    timestamp = Column(DateTime, default=datetime.utcnow) 
+    
+    
+    sender = relationship("User", foreign_keys=[sender_id], back_populates="sent_messages")
+    recipient = relationship("User", foreign_keys=[recipient_id], back_populates="received_messages")
