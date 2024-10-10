b9278aaf9765f1c224135abfaf557944e952cbd0
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-10 18:22:31 -0400

    feat: add database schemas

---
+++ back-end/app/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    public_key: str
+    encrypted_private_key: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class MessageCreate(BaseModel):
+    recipient_id: int
+    encrypted_message: str
+
+class WebSocketMessage(BaseModel):
+    message: str  # Mensagem do WebSocket
+
+class MessageResponse(BaseModel):
+    id: int
+    sender_id: int
+    recipient_id: int
+    encrypted_message: str
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
