aa824278e1d65f29d497ffc6a8c83cd03e5d0a70
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-16 16:23:55 -0400

    fix: change provider order

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,17 +1,23 @@
 import ChatMain from "../components/ChatMain";
 import ChatSideBar from "../components/ChatSideBar";
+import { ChatProvider } from "../hooks/useChats";
+import { CurrentChatProvider } from "../hooks/useCurrentChat";
 
 const Home = () => {
   return (
-    <div
-      className="h-screen w-screen first-line:overflow-hidden flex items-center justify-center"
-      style={{ background: "#edf2f7" }}
-    >
-      <div className="flex h-screen w-screen overflow-hidden">
-        <ChatSideBar />
-        <ChatMain />
-      </div>
-    </div>
+    <CurrentChatProvider>
+      <ChatProvider>
+        <div
+          className="h-screen w-screen first-line:overflow-hidden flex items-center justify-center"
+          style={{ background: "#edf2f7" }}
+        >
+          <div className="flex h-screen w-screen overflow-hidden">
+            <ChatSideBar />
+            <ChatMain />
+          </div>
+        </div>
+      </ChatProvider>
+    </CurrentChatProvider>
   );
 };
 
