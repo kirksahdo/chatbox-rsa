f6f8eb9b0b75c4edad2c43182de207a75607c7a4
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-21 17:26:50 -0400

    feat: add callback in toast

--- front-end/src/hooks/useToast.tsx
+++ front-end/src/hooks/useToast.tsx
@@ -1,6 +1,14 @@
-import { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 import { ToastContextType, ToastProps, ToastType } from "../@types/toast";
 import Toast from "../components/Toast";
+import ToastNotification from "../components/ToastNotification";
 
 const ToastContext = createContext<ToastContextType | null>(null);
 
@@ -9,40 +17,53 @@ export const ToastProvider: React.FC<{ children: ReactNode }> = ({
 }) => {
   const [toasts, setToasts] = useState<ToastProps[]>([]);
 
-  const addToast = (message: string, type: ToastType) => {
-    const toast: ToastProps = {
-      message,
-      type,
-      onClose: () => {
-        setToasts([...toasts.filter((t) => toast !== t)]);
-      },
-    };
-    setToasts([toast, ...toasts]);
+  const addToast = useCallback(
+    (message: string, type: ToastType, author?: string) => {
+      const toast: ToastProps = {
+        message,
+        type,
+        author,
+        onClose: () => {
+          setToasts([...toasts.filter((t) => toast !== t)]);
+        },
+      };
+      setToasts([toast, ...toasts]);
 
-    setTimeout(() => {
-      if (!toasts.some((t) => toast === t)) toast.onClose();
-    }, 3000);
-  };
+      setTimeout(() => {
+        if (!toasts.some((t) => toast === t)) toast.onClose();
+      }, toast.duration ?? 4000);
+    },
+    [toasts],
+  );
 
   const value = useMemo(
     () => ({
       addToast,
     }),
-    [toasts],
+    [addToast],
   );
 
   return (
     <ToastContext.Provider value={value}>
       {children}
       <div className="fixed top-4 right-4 space-y-3 z-50">
-        {toasts.map((t, i) => (
-          <Toast
-            message={t.message}
-            onClose={t.onClose}
-            type={t.type}
-            key={i}
-          />
-        ))}
+        {toasts.map((t, i) =>
+          t.type === "notification" ? (
+            <ToastNotification
+              message={t.message}
+              author={t.author ?? "none"}
+              onClose={t.onClose}
+              key={i}
+            />
+          ) : (
+            <Toast
+              message={t.message}
+              onClose={t.onClose}
+              type={t.type}
+              key={i}
+            />
+          ),
+        )}
       </div>
     </ToastContext.Provider>
   );
