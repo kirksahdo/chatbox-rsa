503ea0aca39ec2535bb124b7604e410c72700d60
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-25 03:22:23 -0400

    feat: add message timestamp

--- front-end/src/components/cards/ReceivedMessageCard.tsx
+++ front-end/src/components/cards/ReceivedMessageCard.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Message } from "../../@types/chat";
+import moment from "moment";
 
 const ReceivedMessageCard: React.FC<{ message: Message }> = ({ message }) => {
   return (
     <div className="flex mb-4 cursor-pointer">
-      <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
+      {/* <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
         <img
           src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
           alt="User Avatar"
           className="w-8 h-8 rounded-full"
         />
-      </div>
-      <div className="flex max-w-96 bg-white rounded-lg p-3 gap-3">
+      </div> */}
+      <div className="flex max-w-96 bg-white rounded-lg p-3 flex-col">
         <p style={{ wordBreak: "break-all" }} className="text-gray-700">
           {message.encrypted_message}
+        </p>
+        <p className="text-xs text-gray-400">
+          {moment(message.timestamp).format("HH:mm DD/mm/YYYY")}
         </p>
       </div>
     </div>
--- front-end/src/components/cards/SendedMessageCard.tsx
+++ front-end/src/components/cards/SendedMessageCard.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Message } from "../../@types/chat";
+import moment from "moment";
 
 const SendedMessageCard: React.FC<{ message: Message }> = ({ message }) => {
   return (
     <div className="flex justify-end mb-4 cursor-pointer">
-      <div className="flex max-w-96 flex-wrap bg-indigo-500 text-white rounded-lg p-3 gap-3">
+      <div className="flex max-w-96 flex-wrap bg-indigo-500 text-white rounded-lg p-3 flex-col">
         <p style={{ wordBreak: "break-all" }}>{message.encrypted_message}</p>
+        <p className="text-xs text-gray-400">
+          {moment(message.timestamp).format("HH:mm DD/mm/YYYY")}
+        </p>
       </div>
-      <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
+      {/* <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
         <img
           src="https://placehold.co/200x/b7a8ff/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
           alt="My Avatar"
           className="w-8 h-8 rounded-full"
         />
-      </div>
+      </div> */}
     </div>
   );
 };
