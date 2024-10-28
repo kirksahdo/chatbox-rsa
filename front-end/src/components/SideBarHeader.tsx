220465b728faab1f6f3aebe935d97a19b629ff7f
Kirk Sahdo <ksahdo10@gmail.com>
2024-10-28 14:55:50 -0400

    style: change side bar header menu z-index value

--- front-end/src/components/SideBarHeader.tsx
+++ front-end/src/components/SideBarHeader.tsx
@@ -30,7 +30,7 @@ const SideBarHeader = () => {
         {showMenu && (
           <div
             id="menuDropdown"
-            className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-50"
+            className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-30"
           >
             <ul className="py-2 px-3">
               <li>
